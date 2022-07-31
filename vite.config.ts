95c0deac51262da5f7dd1c3c895c1e7d3d7d38ce
raggebatman <raggebatman@tutanota.com>
2022-07-31 15:13:24 +0200

    expose server in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-const production = process.env.NODE_ENV === 'production';
+const production: boolean = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // expose the server when in development
+  server: { 
+    host: !production 
+  },
   plugins: [ 
     svelte({
       emitCss: production,
