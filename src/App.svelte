e2c24c6369e54543d92755b7ae3520709b7644b4
raggebatman <raggebatman@tutanota.com>
2022-07-31 15:12:54 +0200

    fixed the css

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <main>
-	<h1>Hey,</h1>
-	<p>
-		You're seeing the Svelte + TypeScript + Vite + Sass template.
-	</p>
-	<p>
-		This is just an example app.
-		<br>
-		Feel free to modify it to your heart's content.
-	</p>
+	<div>
+		<h1>Hey,</h1>
+		<p>
+			You're seeing the Svelte + TypeScript + Vite + Sass template.
+		</p>
+		<p>
+			This is just an example app.
+			<br>
+			Feel free to modify it to your heart's content.
+		</p>
+	</div>
 </main>
